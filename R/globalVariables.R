692722f959c3f53a7cf29a9bb690596719cff24b
Rachel Rosenthal <rachel.rosenthal.14@ucl.ac.uk>
2016-01-27 16:52:11 +0000

    Boring updates

--- R/globalVariables.R
+++ R/globalVariables.R
@@ -1,1 +1,1 @@
-if(getRversion() >= "2.15.1")  utils::globalVariables(c("signatures.nature2013", "signatures.cosmic"))
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("signatures.nature2013", "signatures.cosmic", "tri.counts.exome", "tri.counts.genome"))
